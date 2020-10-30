1bff5bae3d97bc21487eff5b59596896360bb1b0
Nik <nikita.leksikov@shpock.com>
2020-10-30 23:58:38 +0100

    update proto file, regenerate *.pb.go files

--- places.pb.go
+++ places.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.13.0
 // source: places.proto
 
-package places
+package api_grpc
 
 import (
 	proto "github.com/golang/protobuf/proto"
@@ -107,7 +107,7 @@ type Place struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Id          int32                `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	Id          int64                `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 	Name        string               `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
 	Location    string               `protobuf:"bytes,3,opt,name=location,proto3" json:"location,omitempty"`
 	Type        Place_PlaceType      `protobuf:"varint,4,opt,name=type,proto3,enum=places.Place_PlaceType" json:"type,omitempty"`
@@ -147,7 +147,7 @@ func (*Place) Descriptor() ([]byte, []int) {
 	return file_places_proto_rawDescGZIP(), []int{0}
 }
 
-func (x *Place) GetId() int32 {
+func (x *Place) GetId() int64 {
 	if x != nil {
 		return x.Id
 	}
@@ -189,16 +189,17 @@ func (x *Place) GetLastUpdated() *timestamp.Timestamp {
 	return nil
 }
 
-type Places struct {
+// The request message
+type PlaceIdRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Places []*Place `protobuf:"bytes,1,rep,name=places,proto3" json:"places,omitempty"`
+	Id int64 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 }
 
-func (x *Places) Reset() {
-	*x = Places{}
+func (x *PlaceIdRequest) Reset() {
+	*x = PlaceIdRequest{}
 	if protoimpl.UnsafeEnabled {
 		mi := &file_places_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -206,13 +207,13 @@ func (x *Places) Reset() {
 	}
 }
 
-func (x *Places) String() string {
+func (x *PlaceIdRequest) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*Places) ProtoMessage() {}
+func (*PlaceIdRequest) ProtoMessage() {}
 
-func (x *Places) ProtoReflect() protoreflect.Message {
+func (x *PlaceIdRequest) ProtoReflect() protoreflect.Message {
 	mi := &file_places_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -224,12 +225,107 @@ func (x *Places) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use Places.ProtoReflect.Descriptor instead.
-func (*Places) Descriptor() ([]byte, []int) {
+// Deprecated: Use PlaceIdRequest.ProtoReflect.Descriptor instead.
+func (*PlaceIdRequest) Descriptor() ([]byte, []int) {
 	return file_places_proto_rawDescGZIP(), []int{1}
 }
 
-func (x *Places) GetPlaces() []*Place {
+func (x *PlaceIdRequest) GetId() int64 {
+	if x != nil {
+		return x.Id
+	}
+	return 0
+}
+
+// The response message
+type PlaceResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Message string `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
+}
+
+func (x *PlaceResponse) Reset() {
+	*x = PlaceResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_places_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PlaceResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PlaceResponse) ProtoMessage() {}
+
+func (x *PlaceResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_places_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PlaceResponse.ProtoReflect.Descriptor instead.
+func (*PlaceResponse) Descriptor() ([]byte, []int) {
+	return file_places_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *PlaceResponse) GetMessage() string {
+	if x != nil {
+		return x.Message
+	}
+	return ""
+}
+
+type PlacesResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Places []*Place `protobuf:"bytes,1,rep,name=places,proto3" json:"places,omitempty"`
+}
+
+func (x *PlacesResponse) Reset() {
+	*x = PlacesResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_places_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PlacesResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PlacesResponse) ProtoMessage() {}
+
+func (x *PlacesResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_places_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PlacesResponse.ProtoReflect.Descriptor instead.
+func (*PlacesResponse) Descriptor() ([]byte, []int) {
+	return file_places_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *PlacesResponse) GetPlaces() []*Place {
 	if x != nil {
 		return x.Places
 	}
@@ -243,7 +339,7 @@ var file_places_proto_rawDesc = []byte{
 	0x70, 0x6c, 0x61, 0x63, 0x65, 0x73, 0x1a, 0x1f, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2f, 0x70,
 	0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d,
 	0x70, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0xea, 0x02, 0x0a, 0x05, 0x50, 0x6c, 0x61, 0x63,
-	0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x02, 0x69,
+	0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x02, 0x69,
 	0x64, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
 	0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x1a, 0x0a, 0x08, 0x6c, 0x6f, 0x63, 0x61, 0x74, 0x69, 0x6f,
 	0x6e, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x6c, 0x6f, 0x63, 0x61, 0x74, 0x69, 0x6f,
@@ -265,10 +361,22 @@ var file_places_proto_rawDesc = []byte{
 	0x07, 0x12, 0x09, 0x0a, 0x05, 0x52, 0x49, 0x56, 0x45, 0x52, 0x10, 0x08, 0x12, 0x0c, 0x0a, 0x08,
 	0x4d, 0x4f, 0x55, 0x4e, 0x54, 0x41, 0x49, 0x4e, 0x10, 0x09, 0x12, 0x0b, 0x0a, 0x07, 0x53, 0x54,
 	0x41, 0x54, 0x49, 0x4f, 0x4e, 0x10, 0x0a, 0x12, 0x0b, 0x0a, 0x07, 0x4f, 0x55, 0x54, 0x44, 0x4f,
-	0x4f, 0x52, 0x10, 0x0b, 0x22, 0x2f, 0x0a, 0x06, 0x50, 0x6c, 0x61, 0x63, 0x65, 0x73, 0x12, 0x25,
-	0x0a, 0x06, 0x70, 0x6c, 0x61, 0x63, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x0d,
-	0x2e, 0x70, 0x6c, 0x61, 0x63, 0x65, 0x73, 0x2e, 0x50, 0x6c, 0x61, 0x63, 0x65, 0x52, 0x06, 0x70,
-	0x6c, 0x61, 0x63, 0x65, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x4f, 0x52, 0x10, 0x0b, 0x22, 0x20, 0x0a, 0x0e, 0x50, 0x6c, 0x61, 0x63, 0x65, 0x49, 0x64, 0x52,
+	0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x03, 0x52, 0x02, 0x69, 0x64, 0x22, 0x29, 0x0a, 0x0d, 0x50, 0x6c, 0x61, 0x63, 0x65, 0x52,
+	0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61,
+	0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67,
+	0x65, 0x22, 0x37, 0x0a, 0x0e, 0x50, 0x6c, 0x61, 0x63, 0x65, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6f,
+	0x6e, 0x73, 0x65, 0x12, 0x25, 0x0a, 0x06, 0x70, 0x6c, 0x61, 0x63, 0x65, 0x73, 0x18, 0x01, 0x20,
+	0x03, 0x28, 0x0b, 0x32, 0x0d, 0x2e, 0x70, 0x6c, 0x61, 0x63, 0x65, 0x73, 0x2e, 0x50, 0x6c, 0x61,
+	0x63, 0x65, 0x52, 0x06, 0x70, 0x6c, 0x61, 0x63, 0x65, 0x73, 0x32, 0x44, 0x0a, 0x06, 0x50, 0x6c,
+	0x61, 0x63, 0x65, 0x73, 0x12, 0x3a, 0x0a, 0x07, 0x47, 0x65, 0x74, 0x42, 0x79, 0x49, 0x64, 0x12,
+	0x16, 0x2e, 0x70, 0x6c, 0x61, 0x63, 0x65, 0x73, 0x2e, 0x50, 0x6c, 0x61, 0x63, 0x65, 0x49, 0x64,
+	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x15, 0x2e, 0x70, 0x6c, 0x61, 0x63, 0x65, 0x73,
+	0x2e, 0x50, 0x6c, 0x61, 0x63, 0x65, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00,
+	0x42, 0x25, 0x5a, 0x23, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6e,
+	0x69, 0x6b, 0x6c, 0x65, 0x6b, 0x2f, 0x61, 0x70, 0x69, 0x2d, 0x67, 0x72, 0x70, 0x63, 0x3b, 0x61,
+	0x70, 0x69, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -284,19 +392,23 @@ func file_places_proto_rawDescGZIP() []byte {
 }
 
 var file_places_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
-var file_places_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_places_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_places_proto_goTypes = []interface{}{
 	(Place_PlaceType)(0),        // 0: places.Place.PlaceType
 	(*Place)(nil),               // 1: places.Place
-	(*Places)(nil),              // 2: places.Places
-	(*timestamp.Timestamp)(nil), // 3: google.protobuf.Timestamp
+	(*PlaceIdRequest)(nil),      // 2: places.PlaceIdRequest
+	(*PlaceResponse)(nil),       // 3: places.PlaceResponse
+	(*PlacesResponse)(nil),      // 4: places.PlacesResponse
+	(*timestamp.Timestamp)(nil), // 5: google.protobuf.Timestamp
 }
 var file_places_proto_depIdxs = []int32{
 	0, // 0: places.Place.type:type_name -> places.Place.PlaceType
-	3, // 1: places.Place.lastUpdated:type_name -> google.protobuf.Timestamp
-	1, // 2: places.Places.places:type_name -> places.Place
-	3, // [3:3] is the sub-list for method output_type
-	3, // [3:3] is the sub-list for method input_type
+	5, // 1: places.Place.lastUpdated:type_name -> google.protobuf.Timestamp
+	1, // 2: places.PlacesResponse.places:type_name -> places.Place
+	2, // 3: places.Places.GetById:input_type -> places.PlaceIdRequest
+	3, // 4: places.Places.GetById:output_type -> places.PlaceResponse
+	4, // [4:5] is the sub-list for method output_type
+	3, // [3:4] is the sub-list for method input_type
 	3, // [3:3] is the sub-list for extension type_name
 	3, // [3:3] is the sub-list for extension extendee
 	0, // [0:3] is the sub-list for field type_name
@@ -321,7 +433,31 @@ func file_places_proto_init() {
 			}
 		}
 		file_places_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Places); i {
+			switch v := v.(*PlaceIdRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_places_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PlaceResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_places_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PlacesResponse); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -339,9 +475,9 @@ func file_places_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_places_proto_rawDesc,
 			NumEnums:      1,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
-			NumServices:   0,
+			NumServices:   1,
 		},
 		GoTypes:           file_places_proto_goTypes,
 		DependencyIndexes: file_places_proto_depIdxs,
